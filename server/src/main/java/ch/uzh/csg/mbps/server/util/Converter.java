9b6e74e7bbdfb1c668294d92a6bfde5ab4518615
Jeton Memeti <jeton.memeti@gmail.com>
2014-06-09 12:21:12 +0200

    Added TODOs to Converter.

--- server/src/main/java/ch/uzh/csg/mbps/server/util/Converter.java
+++ server/src/main/java/ch/uzh/csg/mbps/server/util/Converter.java
@@ -2,9 +2,14 @@ package ch.uzh.csg.mbps.server.util;
 
 import java.math.BigDecimal;
 
+//TODO: javadoc
 public class Converter {
 	
-	public static BigDecimal getTransactionLongInBigDecimal(Long longValue){
+	//TODO jeton: move to shared resources?
+	
+	//TODO jeton: bigdecimal to long
+	
+	public static BigDecimal getTransactionLongInBigDecimal(long longValue){
 		BigDecimal bigDecimalValue = new BigDecimal(longValue);
 		bigDecimalValue.divide(new BigDecimal(100000000));
 		return bigDecimalValue;
