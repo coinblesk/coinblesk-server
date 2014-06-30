8bd63e1469153929f8581539eb9603d44fe06b49
malib <mehmetali.bekooglu@uzh.ch>
2014-06-30 10:39:02 +0200

    server account controller for the client (android)

---
+++ server/src/main/java/ch/uzh/csg/mbps/server/controller/ServerAccountController.java
@@ -0,0 +1,16 @@
+package ch.uzh.csg.mbps.server.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * REST Controller for client http requests regarding ServerAccount operations.
+ * 
+ */
+@Controller
+@RequestMapping("/server_account")
+public class ServerAccountController {
+
+	//TODO: method: only a method for trust relations
+	
+}
