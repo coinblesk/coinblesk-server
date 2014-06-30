87c9d0c71dcbbb2f6d91ad3f38baa1e48d5de355
malib <mehmetali.bekooglu@uzh.ch>
2014-06-30 10:18:31 +0200

    fixed mistakes

--- server/src/main/java/ch/uzh/csg/mbps/server/service/ServerAccountService.java
+++ server/src/main/java/ch/uzh/csg/mbps/server/service/ServerAccountService.java
@@ -1,8 +1,12 @@
 package ch.uzh.csg.mbps.server.service;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import org.hibernate.HibernateException;
+import org.springframework.stereotype.Service;
 
 import ch.uzh.csg.mbps.server.clientinterface.IServerAccount;
 import ch.uzh.csg.mbps.server.dao.ServerAccountDAO;
@@ -11,6 +15,7 @@ import ch.uzh.csg.mbps.server.util.BitcoindController;
 import ch.uzh.csg.mbps.server.util.Config;
 import ch.uzh.csg.mbps.server.util.exceptions.BalanceNotZeroException;
 import ch.uzh.csg.mbps.server.util.exceptions.InvalidEmailException;
+import ch.uzh.csg.mbps.server.util.exceptions.InvalidPublicKeyException;
 import ch.uzh.csg.mbps.server.util.exceptions.InvalidUrlException;
 import ch.uzh.csg.mbps.server.util.exceptions.ServerAccountNotFoundException;
 import ch.uzh.csg.mbps.server.util.exceptions.UrlAlreadyExistsException;
@@ -21,6 +26,7 @@ import com.azazar.bitcoin.jsonrpcclient.BitcoinException;
  * Service class for {@link ServerAccount}.
  *
  */
+@Service
 public class ServerAccountService implements IServerAccount{
 	private static ServerAccountService serverAccountService;
 	private static boolean TESTING_MODE = false;
@@ -60,11 +66,20 @@ public class ServerAccountService implements IServerAccount{
 	}
 	
 	@Override
-	public boolean createAccount(ServerAccount serverAccount) throws UrlAlreadyExistsException, BitcoinException, InvalidUrlException, InvalidEmailException {
-		if (TESTING_MODE)
-			return createAccount(serverAccount, "fake-address");
-		else
-			return createAccount(serverAccount, getNewPayinAddress());
+	public boolean createAccount(ServerAccount serverAccount) throws UrlAlreadyExistsException, BitcoinException, InvalidUrlException, InvalidEmailException, InvalidPublicKeyException {
+		Date date = new Date();
+		if (TESTING_MODE){
+			String strDate = "2014-06-19 14:35:54.0";
+			try {
+				Date date2 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(strDate);
+				return createAccount(serverAccount, "fake-address", date2);
+			} catch (ParseException e) {
+				e.printStackTrace();
+				return createAccount(serverAccount, "fake-address", date);
+			}
+		} else {
+			return createAccount(serverAccount, getNewPayinAddress(), date);
+		}
 	}
 
 	/**
@@ -76,11 +91,13 @@ public class ServerAccountService implements IServerAccount{
 	 * @throws BitcoinException
 	 * @throws InvalidUrlException
 	 * @throws InvalidEmailException
+	 * @throws InvalidPublicKeyException 
 	 */
-	private boolean createAccount(ServerAccount serverAccount, String payinAddress) throws UrlAlreadyExistsException, BitcoinException, InvalidUrlException, InvalidEmailException {
+	private boolean createAccount(ServerAccount serverAccount, String payinAddress, Date date) throws UrlAlreadyExistsException, BitcoinException, InvalidUrlException, InvalidEmailException, InvalidPublicKeyException {
 		serverAccount.setUrl(serverAccount.getUrl().trim());
 		String url = serverAccount.getUrl();
 		String email = serverAccount.getEmail();
+		String publicKey = serverAccount.getPublicKey();
 		
 		if (url == null)
 			throw new InvalidUrlException();
@@ -88,6 +105,9 @@ public class ServerAccountService implements IServerAccount{
 		if (email == null)
 			throw new InvalidEmailException();
 
+		if (publicKey == null)
+			throw new InvalidPublicKeyException();
+		
 		if (!url.matches(Config.URL_REGEX))
 			throw new InvalidUrlException();
 		
@@ -105,6 +125,7 @@ public class ServerAccountService implements IServerAccount{
 		serverAccount = new ServerAccount(serverAccount.getUrl(), serverAccount.getEmail(), serverAccount.getPublicKey());
 		
 		serverAccount.setPayinAddress(payinAddress);
+		serverAccount.setCreationDate(date);
 		
 		String token = java.util.UUID.randomUUID().toString();
 		try {
@@ -143,8 +164,11 @@ public class ServerAccountService implements IServerAccount{
 		if (updatedAccount.getUrl() != null && !updatedAccount.getUrl().isEmpty())
 			serverAccount.setUrl(updatedAccount.getUrl());
 		
+		if (updatedAccount.getTrustLevel() != serverAccount.getTrustLevel())
+			serverAccount.setTrustLevel(updatedAccount.getTrustLevel());
+		
 		try{
-			ServerAccountDAO.updatedAccount(updatedAccount);
+			ServerAccountDAO.updatedAccount(serverAccount);
 		} catch (HibernateException e) {
 			return false;
 		}
