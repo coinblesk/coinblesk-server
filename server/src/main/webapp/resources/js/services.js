8c39482a01a3a70ce5b1e57b4ec2a92fc7dc318a
malib <mehmetali.bekooglu@uzh.ch>
2014-07-30 15:06:36 +0200

    add new service access token cookie service

--- server/src/main/webapp/resources/js/services.js
+++ server/src/main/webapp/resources/js/services.js
@@ -6,6 +6,33 @@ var AppServices = angular.module('SpringAngularApp.services', []);
 
 AppServices.value('version', '0.1');
 
+AppServices.service('accessTokenService', function($cookies, $cookieStore){
+	return ({
+		initToken: initToken,
+		removeToken: removeToken,
+		initCookies: initCookies
+	});
+	
+	function initToken(token){
+		httpHeaders.common['Authorization'] = token || $cookies.token;
+	};
+	
+	function initCookies() {
+		var jsessionid = $cookies.JSESSIONID;
+		$cookieStore.put('JSESSIONID', jsessionid);
+		console.log("Services " + jsessionid);
+	};
+	
+	function removeToken() {
+		httpHeaders.common['Authorization'] = null;
+	};
+	
+	function removeCookies() {
+		delete $cookie.JSESSIONID;
+		$cookieStore.remove('JSESSIONID');
+	};
+});
+
 AppServices.service('balanceService', function($http, $q) {
 	return({
 		getBalance: getBalance
@@ -14,10 +41,7 @@ AppServices.service('balanceService', function($http, $q) {
 	function getBalance(){
 		var request = $http({
 			method: 'GET', 
-			url: 'home/balance',
-			params : {
-				action: 'GET'
-			}
+			url: 'home/balance'
 		});
 		
 		return( request.then(handleSuccess, handleError));
@@ -43,10 +67,7 @@ AppServices.factory('serverTransactionsFactory', function($http, $q) {
 	serverTransactionsFactory.getLastThreeTransactions = function() {
 		var request = $http({
 			method: 'GET',
-			url: 'home/lastThreeTransaction',
-			params: {
-				action: 'GET'
-			}
+			url: 'home/lastThreeTransaction'
 		});
 		return (request.then(serverTransactionsFactory.handleSucess, serverTransactionsFactory.handleError));
 	};
@@ -54,10 +75,7 @@ AppServices.factory('serverTransactionsFactory', function($http, $q) {
 	serverTransactionsFactory.getHistory = function() {
 		var request = $http({
 			method: 'GET',
-			url: "history/transactions",
-			params: {
-				action: 'GET'
-			}
+			url: "history/transactions"
 		});
 		return (request.then(serverTransactionsFactory.handleSuccess, serverTransactionsFactory.handleError));
 	};
@@ -84,10 +102,7 @@ AppServices.factory('userAccountFactory', function($http, $q) {
 	userAccountFactory.getAdmins = function() {
 		var request = $http({
 			method: 'GET',
-			url: 'home/admins',
-			params: {
-				action: 'GET'
-			}
+			url: 'home/admins'
 		});
 		return (request.then(userAccountFactory.handleSuccess, userAccountFactory.handleError));
 	};
@@ -95,10 +110,7 @@ AppServices.factory('userAccountFactory', function($http, $q) {
 	userAccountFactory.getUsers = function() {
 		var request = $http({
 			method: 'GET',
-			url: 'users/all',
-			params: {
-				action: 'GET'
-			}
+			url: 'users/all'
 		});
 		return (request.then(userAccountFactory.handleSuccess, userAccountFactory.handleError));
 	};
@@ -125,10 +137,7 @@ AppServices.factory('serverAccountFactory', function($http, $q) {
 	serverAccountFactory.getAccounts = function() {
 		var request = $http({
 			method: 'GET',
-			url: 'relation/accounts',
-			params: {
-				action: 'GET'
-			}
+			url: 'relation/accounts'
 		});
 		return (request.then(serverAccountFactory.handleSuccess, serverAccountFactory.handleError));
 	};
@@ -149,20 +158,24 @@ AppServices.factory('serverAccountFactory', function($http, $q) {
 	return serverAccountFactory;
 });
 
-//TODO: mehmet set link
-AppServices.service('Base64Service', function () {
-    var keyStr = "ABCDEFGHIJKLMNOP" +
-        "QRSTUVWXYZabcdef" +
-        "ghijklmnopqrstuv" +
-        "wxyz0123456789+/" +
-        "=";
-    this.encode = function (input) {
-        var output = "",
-            chr1, chr2, chr3 = "",
-            enc1, enc2, enc3, enc4 = "",
-            i = 0;
+//TODO: mehmet set link http://wemadeyoulook.at/en/blog/implementing-basic-http-authentication-http-requests-angular/
+AppServices.factory('base64Factory', function() {
+	
+	var base64Factory = {};
+	
+	var keyStr = 'ABCDEFGHIJKLMNOP' +
+        'QRSTUVWXYZabcdef' +
+        'ghijklmnopqrstuv' +
+        'wxyz0123456789+/' +
+        '=';
+    
+	base64Factory.encode = function (input) {
+        var output = "";
+        var chr1, chr2, chr3 = "";
+        var enc1, enc2, enc3, enc4 = "";
+        var i = 0;
 
-        while (i < input.length) {
+        do {
             chr1 = input.charCodeAt(i++);
             chr2 = input.charCodeAt(i++);
             chr3 = input.charCodeAt(i++);
@@ -185,21 +198,27 @@ AppServices.service('Base64Service', function () {
                 keyStr.charAt(enc4);
             chr1 = chr2 = chr3 = "";
             enc1 = enc2 = enc3 = enc4 = "";
-        }
+        } while (i < input.length);
 
         return output;
-    };
-
-    this.decode = function (input) {
-        var output = "",
-            chr1, chr2, chr3 = "",
-            enc1, enc2, enc3, enc4 = "",
-            i = 0;
+	};
+	
+	base64Factory.decode = function (input) {
+        var output = "";
+        var chr1, chr2, chr3 = "";
+        var enc1, enc2, enc3, enc4 = "";
+        var i = 0;
 
         // remove all characters that are not A-Z, a-z, 0-9, +, /, or =
+        var base64test = /[^A-Za-z0-9\+\/\=]/g;
+        if (base64test.exec(input)) {
+            alert("There were invalid base64 characters in the input text.\n" +
+                "Valid base64 characters are A-Z, a-z, 0-9, '+', '/',and '='\n" +
+                "Expect errors in decoding.");
+        }
         input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
 
-        while (i < input.length) {
+        do {
             enc1 = keyStr.indexOf(input.charAt(i++));
             enc2 = keyStr.indexOf(input.charAt(i++));
             enc3 = keyStr.indexOf(input.charAt(i++));
@@ -220,6 +239,11 @@ AppServices.service('Base64Service', function () {
 
             chr1 = chr2 = chr3 = "";
             enc1 = enc2 = enc3 = enc4 = "";
-        }
+
+        } while (i < input.length);
+
+        return output;
     };
+    
+    return base64Factory;
 });
