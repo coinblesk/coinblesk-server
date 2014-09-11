16ccf6e02f7438f0849a6d22ba4ec42c04f5e427
malib <mehmetali.bekooglu@uzh.ch>
2014-09-11 07:42:30 +0200

    updated methods

--- server/src/main/java/ch/uzh/csg/mbps/server/controllerui/ServerCommunicationController.java
+++ server/src/main/java/ch/uzh/csg/mbps/server/controllerui/ServerCommunicationController.java
@@ -2,6 +2,7 @@ package ch.uzh.csg.mbps.server.controllerui;
 
 import java.util.Calendar;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,13 +13,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import ch.uzh.csg.mbps.customserialization.PKIAlgorithm;
 import ch.uzh.csg.mbps.customserialization.exceptions.UnknownPKIAlgorithmException;
 import ch.uzh.csg.mbps.keys.CustomPublicKey;
-import ch.uzh.csg.mbps.responseobject.CreateServerAccountObject;
+import ch.uzh.csg.mbps.responseobject.CreateSAObject;
+import ch.uzh.csg.mbps.responseobject.ServerAccountObject;
+import ch.uzh.csg.mbps.responseobject.TransferObject;
+import ch.uzh.csg.mbps.server.clientinterface.IActivities;
 import ch.uzh.csg.mbps.server.clientinterface.IServerAccount;
 import ch.uzh.csg.mbps.server.clientinterface.IUserAccount;
 import ch.uzh.csg.mbps.server.domain.ServerAccount;
+import ch.uzh.csg.mbps.server.util.ActivitiesTitle;
 import ch.uzh.csg.mbps.server.util.Config;
 import ch.uzh.csg.mbps.server.util.Constants;
 import ch.uzh.csg.mbps.server.util.SecurityConfig;
+import ch.uzh.csg.mbps.server.util.exceptions.BalanceNotZeroException;
 import ch.uzh.csg.mbps.server.util.exceptions.InvalidEmailException;
 import ch.uzh.csg.mbps.server.util.exceptions.InvalidUrlException;
 import ch.uzh.csg.mbps.server.util.exceptions.ServerAccountNotFoundException;
@@ -31,15 +37,18 @@ import com.azazar.bitcoin.jsonrpcclient.BitcoinException;
 @RequestMapping("/communication")
 public class ServerCommunicationController {
 
-	@Autowired
-	IServerAccount serverAccountService;
-	@Autowired
-	IUserAccount userAccountService;
+	 protected static Logger logger = Logger.getLogger("controller");
 	
-	@RequestMapping(value = "/createNewAccount", method = RequestMethod.POST, produces = "application/json")
-	@ResponseBody
-	public CreateServerAccountObject createNewAccount(@RequestBody CreateServerAccountObject request){
-		CreateServerAccountObject response = new CreateServerAccountObject();
+	@Autowired
+	private IServerAccount serverAccountService;
+	@Autowired
+	private IUserAccount userAccountService;
+	@Autowired
+	private IActivities activitiesService;
+	
+	@RequestMapping(value = "/createNewAccount", method = RequestMethod.POST, consumes="application/json", produces = "application/json")
+	@ResponseBody public CreateSAObject createNewAccount(@RequestBody CreateSAObject request){
+		CreateSAObject response = new CreateSAObject();
 		PKIAlgorithm pkiAlgorithm;
 		ServerAccount account;
 		
@@ -61,27 +70,26 @@ public class ServerCommunicationController {
 				newAccount.setDeleted(false);
 				newAccount.setEmail(request.getEmail());
 				
-				if(newAccount.getUrl() == null){
-					
-				}
 				boolean success = serverAccountService.persistAccount(newAccount);
 				if(!success){
 					response.setMessage("Failed to create Server Account");
 					response.setSuccessful(false);
 				}
-				response.setMessage(Config.ACCOUNT_SUCCESS);
 			}
 			// retrieve existing account and added public key to the server account
 			account = serverAccountService.getByUrl(request.getUrl());
 			serverAccountService.saveServerPublicKey(account.getId(), pkiAlgorithm, request.getCustomPublicKey().getPublicKey());
-			response.setSuccessful(true);
+			
 
 			String serverUrl = SecurityConfig.BASE_URL;
 			String email = userAccountService.getAdminEmail();
 
 			CustomPublicKey cpk = new CustomPublicKey(Constants.SERVER_KEY_PAIR.getKeyNumber(), Constants.SERVER_KEY_PAIR.getPkiAlgorithm(), Constants.SERVER_KEY_PAIR.getPublicKey());
-			CreateServerAccountObject create = new CreateServerAccountObject(serverUrl,email);
-			create.setCustomPublicKey(cpk);
+			response.setUrl(serverUrl);
+			response.setEmail(email);
+			response.setCustomPublicKey(cpk);
+			response.setMessage(Config.ACCOUNT_SUCCESS);
+			response.setSuccessful(true);
 
 			return response;
 		} 
@@ -116,5 +124,122 @@ public class ServerCommunicationController {
 			response.setMessage(e.getMessage());
 			return response;
 		}	
+	}
+
+	@RequestMapping(value = "/createNewAccountData", method = RequestMethod.POST, produces = "application/json")
+	@ResponseBody public ServerAccountObject createNewAccountData(@RequestBody ServerAccountObject request){
+		ServerAccountObject response = new ServerAccountObject();
+		ServerAccount account;
+		
+		try {
+			//check if account exists already
+			if(serverAccountService.checkIfExistsByUrl(request.getUrl())){
+				if(!serverAccountService.isDeletedByUrl(request.getUrl())){
+					account = serverAccountService.getByUrl(request.getUrl());
+					account.setPayoutAddress(request.getPayoutAddress());
+					boolean success = serverAccountService.updateAccount(request.getUrl(), account);
+					if(success){
+						String serverUrl = SecurityConfig.BASE_URL;
+						response.setUrl(serverUrl);
+						response.setPayoutAddress(account.getPayinAddress());
+						response.setSuccessful(true);
+						response.setMessage(Config.ACCOUNT_SUCCESS);
+					} else {
+						response.setSuccessful(false);
+						response.setMessage(Config.ACCOUNT_FAILED);
+					}
+				} else {
+					response.setSuccessful(false);
+					response.setMessage(Config.ACCOUNT_FAILED);
+				}			
+			} else {
+				response.setSuccessful(false);
+				response.setMessage(Config.ACCOUNT_FAILED);				
+			}
+			return response;
+		} 
+		catch (ServerAccountNotFoundException e) {
+			response.setSuccessful(false);
+			response.setMessage(e.getMessage());
+			return response;	
+		} 
+	}
+	
+	@RequestMapping(value = "/downgradeTrustLevel", method = RequestMethod.POST,consumes="application/json", produces = "application/json")
+	@ResponseBody public TransferObject downgradeTrustLevel(@RequestBody ServerAccountObject request) throws ServerAccountNotFoundException{
+		TransferObject response = new TransferObject();
+		try{
+			ServerAccount account = serverAccountService.getByUrl(request.getUrl());
+			if(account.getTrustLevel() > request.getTrustLevel()){
+				ServerAccount updated = new ServerAccount();
+				updated.setTrustLevel(request.getTrustLevel());
+				boolean success = serverAccountService.updateAccount(request.getUrl(), updated);
+				if(success){
+					activitiesService.activityLog(Config.NOT_AVAILABLE, ActivitiesTitle.DOWNGRADE_TRUST_LEVEL, "Trust level of Server account "+ request.getUrl() + " is downgraded to " + request.getTrustLevel());
+					response.setSuccessful(true);
+					response.setMessage("Succeeded to downgrade!");
+					return response;
+				}
+			}
+		} catch (ServerAccountNotFoundException e) {
+			response.setSuccessful(false);
+			response.setMessage("Server Account does not exists!");
+			return response;
+		}
+		response.setSuccessful(false);
+		response.setMessage("Failed to downgrade");
+		return response;
+	}
+	
+	@RequestMapping(value = "/upgradeTrustLevel", method = RequestMethod.POST, consumes="application/json", produces = "application/json")
+	@ResponseBody public TransferObject upgradeTrustLevel(@RequestBody ServerAccountObject request) throws ServerAccountNotFoundException{
+		TransferObject response = new TransferObject();
+		try{
+			ServerAccount account = serverAccountService.getByUrl(request.getUrl());
+			if(account.getTrustLevel() < request.getTrustLevel()){
+				//TODO: mehmet make a table which is responsible for yes no answers
+
+				response.setSuccessful(true);
+				response.setMessage("Succeeded to downgrade!");
+				return response;
+			}
+		} catch (ServerAccountNotFoundException e) {
+			response.setSuccessful(false);
+			response.setMessage("Server Account does not exists!");
+			return response;
+		}
+		response.setSuccessful(false);
+		response.setMessage("Failed to upgrade");
+		return response;
+	}
+	
+	@RequestMapping(value ="/communication/deletedAccount", method = RequestMethod.POST, consumes="application/json")
+	@ResponseBody public TransferObject deleteAccount(@RequestBody ServerAccountObject request){
+		TransferObject response = new TransferObject();
+		try{
+			if(!serverAccountService.isDeletedByUrl(request.getUrl())){
+				boolean success = serverAccountService.deleteAccount(request.getUrl());
+				if(success){					
+					response.setSuccessful(true);
+					response.setMessage("Succeeded to deleted!");
+					activitiesService.activityLog(Config.NOT_AVAILABLE, ActivitiesTitle.DELETE_ACCOUNT, "Server account "+ request.getUrl()+ " is deleted");
+					return response;
+				}
+			} else {
+				response.setSuccessful(true);
+				response.setMessage("Already deleted!");
+				return response;
+			}
+		} catch (ServerAccountNotFoundException e) {
+			response.setSuccessful(false);
+			response.setMessage("Server Account does not exists!");
+			return response;
+		} catch (BalanceNotZeroException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		response.setSuccessful(false);
+		response.setMessage("Failed to deleted");
+		return response;
 	}
 }
