178703ca357da38a29fa269f29b15e6a30e43d55
malib <mehmetali.bekooglu@uzh.ch>
2014-09-11 07:34:49 +0200

    updated titles

--- server/src/main/java/ch/uzh/csg/mbps/server/util/ActivitiesTitle.java
+++ server/src/main/java/ch/uzh/csg/mbps/server/util/ActivitiesTitle.java
@@ -1,24 +1,25 @@
 package ch.uzh.csg.mbps.server.util;
 
 public class ActivitiesTitle {
-	public static final String IVITE_ADMIN = "Invitation as administration";
-	public static final String UPDATE_EMAIL = "Email is update";
-	public static final String UPDATE_PASSWORD = "New password is set";
-	public static final String DELETE_ACCOUNT = "Account is deleted";
+	public static final String IVITE_ADMIN = "Admin invitation";
+	public static final String UPDATE_EMAIL = "Email updated";
+	public static final String UPDATE_PASSWORD = "New password set";
+	public static final String DELETE_ACCOUNT = "Account deleted";
 	public static final String INVITE_SERVER_ACCOUNT = "Invite server account";
-	public static final String CREATE_SERVER_ACCOUNT = "Create Server account";
-	public static final String PERSIST_SERVER_ACCOUNT = "Succesfully created new server account";
-	public static final String DECLINE_SERVER_ACCOUNT = "Server account creation declined";
-	public static final String UPGRADE_TRUST_LEVEL = "Updgrade trust level";
+	public static final String CREATE_SERVER_ACCOUNT = "Create server account";
+	public static final String PERSIST_SERVER_ACCOUNT = "Create server account succeeded";
+	public static final String DECLINE_SERVER_ACCOUNT = "Create server account declined";
+	public static final String UPGRADE_TRUST_LEVEL = "Trust level updgraded";
 	public static final String DOWNGRADE_TRUST_LEVEL = "Trust level downgraded";
-	public static final String UPDATE_BALANCE_LIMIT = "Balance limit is updated";
-	public static final String UPDATE_USER_BALANCE_LIMIT = "User balance limit is updated";
-	public static final String REMOVE_SERVER_ACCOUNT = "Server account relation is cancelled";
-	public static final String ACCEPT_UPGRADE_TRUST_LEVEL = "Upgrade accepted";
-	public static final String DECLINE_UPGRADE_TRUST_LEVEL = "Upgrade declined";
-	public static final String CREATE_PAYOUT_RULES = "Created payout rule for bitcoins";
-	public static final String UPDATE_URL = "URL is updated";
-	public static final String PAYOUT_ERROR_BALANCE = "Failed to pay out";
-	public static final String PAYOUT_SUCCEED = "Payout succeded";
+	public static final String UPDATE_BALANCE_LIMIT = "Balance limit updated";
+	public static final String UPDATE_USER_BALANCE_LIMIT = "User balance limit updated";
+	public static final String REMOVE_SERVER_ACCOUNT = "Server account relation deleted";
+	public static final String ACCEPT_UPGRADE_TRUST_LEVEL = "Upgrade trust level accepted";
+	public static final String DECLINE_UPGRADE_TRUST_LEVEL = "Upgrade trust level declined";
+	public static final String CREATE_PAYOUT_RULES = "payout rule created";
+	public static final String UPDATE_URL = "URL updated";
+	public static final String PAYOUT_ERROR_BALANCE = "Pay out failed";
+	public static final String PAYOUT_SUCCEED = "Pay out succeded";
 	public static final String PAYOUT_ERROR_ADDRESS = "Invalid address";
+	public static final String FAILED_CREATE_SERVER_ACCOUNT = "Create server account failed";
 }
