d20cceff375ae0ea8c01df6d2578204fb25a1f54
Thomas Bocek <tom@tomp2p.net>
2016-01-26 15:58:48 +0100

    security and build script updates

--- src/main/java/ch/uzh/csg/coinblesk/server/config/SecurityConfig.java
+++ src/main/java/ch/uzh/csg/coinblesk/server/config/SecurityConfig.java
@@ -7,7 +7,9 @@ package ch.uzh.csg.coinblesk.server.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,22 +17,15 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  *
  * @author Thomas Bocek
  */
+@Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-            .authorizeRequests()
-                .antMatchers("/resources/**").permitAll() 
-                .anyRequest().authenticated()
-                .and()
-            .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
-            .logout()                                    
-                .permitAll();
+        http.authorizeRequests().antMatchers("/user/**", "/u/**").hasRole("USER")
+                .and().formLogin().loginPage("/login");
     }
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
---
+++ src/main/java/ch/uzh/csg/coinblesk/server/controller/UserController.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.uzh.csg.coinblesk.server.controller;
+
+import com.coinblesk.responseobject.ExchangeRateTransferObject;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author draft
+ */
+@RestController
+@RequestMapping(value={"/user", "/u"})
+public class UserController {
+    @RequestMapping(value = "/test", method = RequestMethod.GET, produces = "application/json; charset=UTF-8")
+    @ResponseBody
+    public String getInfo() {
+        return "yes";
+    }
+}
