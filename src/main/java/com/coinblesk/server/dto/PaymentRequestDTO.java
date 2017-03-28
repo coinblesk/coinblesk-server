ca51e229b1ca3507f76a0ec5b0ed33ee477d3630
Sebastian Stephan <sstephan1337@gmail.com>
2017-03-28 21:45:38 +0200

    Make new Signable interface for DTOs which have a public key for signing

--- src/main/java/com/coinblesk/server/dto/PaymentRequestDTO.java
+++ src/main/java/com/coinblesk/server/dto/PaymentRequestDTO.java
@@ -4,9 +4,14 @@ import lombok.Data;
 
 import javax.annotation.Nonnull;
 
-public @Data class PaymentRequestDTO {
+public @Data class PaymentRequestDTO implements Signable {
 	@Nonnull private final String fromPublicKey;
 	@Nonnull private final String toPublicKey;
 	@Nonnull private final Long amount;
 	private final long nonce;
+
+	@Override
+	public String getPublicKeyForSigning() {
+		return this.fromPublicKey;
+	}
 }
