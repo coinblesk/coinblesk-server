412ed9c6f9d09f67b7eccc346079bfe61c5f25d0
Sebastian Stephan <sstephan1337@gmail.com>
2017-04-03 12:20:37 +0200

    Remove unused WebMVC stuff.

--- src/main/java/com/coinblesk/server/config/WebMvcConfiguration.java
+++ src/main/java/com/coinblesk/server/config/WebMvcConfiguration.java
@@ -50,31 +50,7 @@ public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
 	}
 
 	@Override
-	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/login").setViewName("login");
-		registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
-	}
-
-	@Override
-	public void configureViewResolvers(ViewResolverRegistry registry) {
-		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-		// no need for JSP
-		// viewResolver.setViewClass(JstlView.class);
-		// viewResolver.setPrefix("/WEB-INF/views/");
-		// viewResolver.setSuffix(".jsp");
-		viewResolver.setPrefix("/static/");
-		viewResolver.setSuffix(".html");
-
-		registry.viewResolver(viewResolver);
-	}
-
-	/**
-	 * Configure ResourceHandlers to serve static resources like CSS/ Javascript etc...
-	 */
-	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/static/**").addResourceLocations("/static/");
-
 		// swagger configuration:
 		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
 		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
