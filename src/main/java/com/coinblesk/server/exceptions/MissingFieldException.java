50c6be38383ac713f606118963af2723fe872e9c
Sebastian Stephan <sebastian@stephan.li>
2017-04-15 16:09:26 +0200

    Remove ToUtils.

--- src/main/java/com/coinblesk/server/exceptions/MissingFieldException.java
+++ src/main/java/com/coinblesk/server/exceptions/MissingFieldException.java
@@ -2,11 +2,8 @@ package com.coinblesk.server.exceptions;
 
 public class MissingFieldException extends RuntimeException {
 
-	private final String fieldName;
-
 	public MissingFieldException(String fieldName) {
 		super("Field " + fieldName + " is missing");
-		this.fieldName = fieldName;
 	}
 
 }
