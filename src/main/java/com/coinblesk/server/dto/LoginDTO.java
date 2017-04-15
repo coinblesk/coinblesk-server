667ab56f3e60f800053f2dfc9701ba9d4038f7ce
Sebastian Stephan <sebastian@stephan.li>
2017-04-16 01:42:29 +0200

    Remove @Pattern check from LoginDTO to remove dependencies from DTOs.

--- src/main/java/com/coinblesk/server/dto/LoginDTO.java
+++ src/main/java/com/coinblesk/server/dto/LoginDTO.java
@@ -3,14 +3,10 @@ package com.coinblesk.server.dto;
 import lombok.Data;
 
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-
-import static com.coinblesk.server.service.UserAccountService.EMAIL_PATTERN;
 
 @Data
 public class LoginDTO {
 
-	@Pattern(regexp = EMAIL_PATTERN)
 	@NotNull
 	private String username;
 
