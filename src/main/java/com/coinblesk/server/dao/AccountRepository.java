cf2776d6b65f0026f6d628e0c8aa860d9c682d56
Sebastian Stephan <sebastian@stephan.li>
2017-04-23 20:06:27 +0200

    Add DAO method to get accounts which have to be closed soon.

--- src/main/java/com/coinblesk/server/dao/AccountRepository.java
+++ src/main/java/com/coinblesk/server/dao/AccountRepository.java
@@ -3,6 +3,9 @@ package com.coinblesk.server.dao;
 import com.coinblesk.server.entity.Account;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface AccountRepository extends CrudRepository<Account, Long> {
 	Account findByClientPublicKey(final byte[] clientPublicKey);
+	List<Account> findByBroadcastBeforeLessThanAndChannelTransactionNotNull(long timestamp);
 }
