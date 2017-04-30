d8dc755afc9f1f5af79cc63ff089c6352665853a
Sebastian Stephan <sebastian@stephan.li>
2017-04-30 13:06:45 +0200

    Force in-memory db and unittest bitcoin params for coinblesk tests.

--- src/test/java/com/coinblesk/server/utilTest/CoinbleskTest.java
+++ src/test/java/com/coinblesk/server/utilTest/CoinbleskTest.java
@@ -2,11 +2,16 @@ package com.coinblesk.server.utilTest;
 
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
 @Transactional
+@TestPropertySource(properties = {
+	"spring.datasource.url: jdbc:h2:mem:testdb",
+	"bitcoin.net:unittest"
+})
 public abstract class CoinbleskTest {
 }
