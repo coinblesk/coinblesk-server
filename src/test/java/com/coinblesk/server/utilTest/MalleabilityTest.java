ada7c5f396e3aff0f449fb77f609fba4df73ae17
Sebastian Stephan <sebastian@stephan.li>
2017-05-01 19:33:03 +0200

    Show ECDSA malleability.

--- src/test/java/com/coinblesk/server/utilTest/MalleabilityTest.java
+++ src/test/java/com/coinblesk/server/utilTest/MalleabilityTest.java
@@ -1,7 +1,47 @@
 package com.coinblesk.server.utilTest;
 
+import com.coinblesk.server.utils.DTOUtils;
+import org.bitcoinj.core.ECKey;
+import org.bitcoinj.core.Sha256Hash;
+import org.junit.Test;
+import java.math.BigInteger;
+
+import static org.bitcoinj.core.ECKey.*;
+
 /**
- * Created by seebi on 17.04.17.
+ * @author
  */
 public class MalleabilityTest {
+	@Test
+	public void attack() {
+		byte[] message = {0x01, 0x02, 0x03, 0x04};
+		Sha256Hash hash = Sha256Hash.of(message);
+
+		ECKey key = new ECKey();
+		ECDSASignature sig = key.sign(hash);
+
+		// Is valid
+		key.verify(hash, sig);
+
+		// Change signature
+		BigInteger sPrime;
+		if (sig.s.compareTo(ECKey.HALF_CURVE_ORDER) <= 0) {
+			// If below half order set s' = -s mod N
+			sPrime = sig.s.multiply(BigInteger.valueOf(-1)).mod(CURVE.getN());
+		} else {
+			// set s' = N - s
+			sPrime = CURVE.getN().subtract(sig.s);
+		}
+		ECDSASignature sig2 = new ECDSASignature(sig.r, sPrime);
+
+		// Still valid
+		key.verify(hash, sig2);
+
+		// Show that hashes are different
+		String hash1 = DTOUtils.toHex(Sha256Hash.of(sig.encodeToDER()).getBytes());
+		String hash2 = DTOUtils.toHex(Sha256Hash.of(sig2.encodeToDER()).getBytes());
+		assert (!hash1.equals(hash2));
+		System.out.println(hash1);
+		System.out.println(hash2);
+	}
 }
